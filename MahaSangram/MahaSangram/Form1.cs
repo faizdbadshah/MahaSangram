6fd28f599594d5b295c559fda7de8841e53356ab
Faiz Siddiqui <faize.sid@gmail.com>
2014-07-09 01:12:54 +0530

    Front Image (Logo)
    - Add a button and logo
    - Faiz

--- MahaSangram/MahaSangram/Form1.cs
+++ MahaSangram/MahaSangram/Form1.cs
@@ -15,5 +15,10 @@ namespace MahaSangram
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
